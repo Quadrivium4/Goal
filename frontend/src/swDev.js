44d000018a270620d3f1024c564a7b9c01c1471d
Quadrivium <coder.quadrivium4@gmail.com>
2024-10-01 11:55:55 +0200

    no more alert

--- frontend/src/swDev.js
+++ frontend/src/swDev.js
@@ -3,7 +3,7 @@ import { baseUrl } from "./constants";
 export function swDev(){
     let swUrl = `${process.env.PUBLIC_URL}/sw.js`;
     console.log({swUrl})
-    alert(navigator.serviceWorker)
+    //alert(navigator.serviceWorker)
     if(navigator)
     navigator.serviceWorker.register(swUrl).then(response=>{
         console.log(response)
